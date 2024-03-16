e19c1791855d0b0053cd1c4f8174bc3c10a9afd4
mj-end <125467532+Mj-Sharifi@users.noreply.github.com>
2024-03-16 10:25:23 +0330

    Add files via upload

--- src/Utils/Theme.js
+++ src/Utils/Theme.js
@@ -17,4 +17,42 @@ const theme = createTheme({
     },
   },
 });
+theme.typography.h3 = {
+  fontSize: "1.4em",
+  fontWeight: 700,
+  [theme.breakpoints.up("sm")]: {
+    fontSize: "1.75em",
+  },
+  [theme.breakpoints.up("md")]: {
+    fontSize: "2.15em",
+  },
+  [theme.breakpoints.up("lg")]: {
+    fontSize: "2.45em",
+  },
+};
+theme.typography.h6 = {
+  fontSize: "1.1em",
+  fontWeight: "500",
+  lineHeight: "1.4rem",
+  [theme.breakpoints.up("sm")]: {
+    fontSize: "1.2em",
+  },
+  [theme.breakpoints.up("md")]: {
+    fontSize: "1.25em",
+  },
+  [theme.breakpoints.up("lg")]: {
+    fontSize: "1.35em",
+  },
+};
+theme.typography.body1 = {
+  fontSize: "1em",
+  fontWeight: "400",
+  lineHeight: "1.2rem",
+  [theme.breakpoints.up("md")]: {
+    fontSize: "1.05em",
+  },
+  [theme.breakpoints.up("lg")]: {
+    fontSize: "1.1em",
+  },
+};
 export default theme;
